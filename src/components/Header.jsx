1df2156ecf8a787c2d744819810aabcff9bb6185
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-18 17:46:26 +0300

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+function Header() {
+
+  return (
+
+    <header className="header">
+
+
+      <div className="logo">
+        🎮 PlayStoreX
+      </div>
+
+
+      <div className="profile">
+
+        <span>
+          👤 Гость
+        </span>
+
+
+        <span className="balance">
+          💰 0 ₽
+        </span>
+
+      </div>
+
+
+    </header>
+
+  );
+
+}
+
+
+export default Header;
