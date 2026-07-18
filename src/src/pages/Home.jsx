b180964a45979da738465461e2f6b4282ca9c588
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-18 17:33:46 +0300

    Create Home.jsx

---
+++ src/src/pages/Home.jsx
@@ -0,0 +1,47 @@
+function Home() {
+
+  return (
+
+    <main className="home">
+
+      <h1>
+        🎮 PlayStoreX
+      </h1>
+
+      <p>
+        Игровой маркетплейс нового поколения
+      </p>
+
+
+      <div className="cards">
+
+        <div className="card">
+          🎮 Robux
+        </div>
+
+
+        <div className="card">
+          🔥 Игры
+        </div>
+
+
+        <div className="card">
+          🛒 Маркет
+        </div>
+
+      </div>
+
+
+      <button>
+        Начать покупки
+      </button>
+
+
+    </main>
+
+  );
+
+}
+
+
+export default Home;
